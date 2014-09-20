a9f54b0110c705ddbcdffa7a1d0998889aa797bb
Aperçu <bgronon@gmail.com>
2014-09-20 22:07:43 +0200

    feat(gen): generation of project, basic prompts

--- app/index.js
+++ app/index.js
@@ -1,53 +1,76 @@
 'use strict';
 var util = require('util');
+var genUtils = require('../util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
 
 var BangularGenerator = yeoman.generators.Base.extend({
   initializing: function () {
+    this.appname = this.appname || path.basename(process.cwd());
+    this.appname = this._.camelize(this._.slugify(this._.humanize(this.appname)));
+
     this.pkg = require('../package.json');
   },
 
   prompting: function () {
+
     var done = this.async();
+    this.log(yosay('Welcome to the ace Bangular generator!'));
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the ace Bangular generator!'
-    ));
-
-    var prompts = [{
+    this.prompt([{
+      type: 'input',
+      name: 'name',
+      message: 'Your project name',
+      default: this.appname
+    }, {
       type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
+      name: 'mongo',
+      message: 'Would you like to include Mongo ?',
       default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+    }, {
+      type: 'checkbox',
+      name: 'modules',
+      message: 'Which module do you want to load ?',
+      choices: [{
+        value: 'ngAnimate',
+        name: 'angular-animate',
+        checked: true
+      }, {
+        value: 'ngCookies',
+        name: 'angular-cookies',
+        checked: true
+      }, {
+        value: 'ngResource',
+        name: 'angular-resource',
+        checked: true
+      }, {
+        value: 'ngSanitize',
+        name: 'angular-sanitize',
+        checked: false
+      }]
+    }], function (props) {
+      //console.log(props);
+      this.appname = props.name;
+      this.mongo = props.mongo;
 
       done();
     }.bind(this));
   },
 
-  writing: {
-    app: function () {
-      this.dest.mkdir('app');
-      this.dest.mkdir('app/templates');
-
-      this.src.copy('_package.json', 'package.json');
-      this.src.copy('_bower.json', 'bower.json');
-    },
-
-    projectfiles: function () {
-      this.src.copy('editorconfig', '.editorconfig');
-      this.src.copy('jshintrc', '.jshintrc');
-    }
+  generate: function () {
+    this.sourceRoot(path.join(__dirname, './templates'));
+    genUtils.processDirectory(this, '.', '.');
   },
 
   end: function () {
-    this.installDependencies();
+    console.info('\n[Bangular] Installing dependencies\n');
+    this.installDependencies({
+      skipMessage: true,
+      callback: function () {
+        console.info('\n[Bangular] Everything is ready !"');
+      }
+    });
   }
 });
 
