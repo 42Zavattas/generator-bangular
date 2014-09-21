24c20b0f8779ab46cfcadcf053e2d779c6842019
meriadec <meriadec.pillet@gmail.com>
2014-09-21 18:05:15 +0200

    feat(ascii): add desc

--- app/ascii.js
+++ app/ascii.js
@@ -7,7 +7,8 @@ var title =
   '\n    ' + chalk.red('__  __  |  __ `/_  __ \\_  __ `') + chalk.yellow('/  / / /_  /_  __ `/_  ___/ ') +
   '\n    ' + chalk.red('_  /_/ // /_/ /_  / / /  /_/ /') + chalk.yellow('/ /_/ /_  / / /_/ /_  /     ') +
   '\n    ' + chalk.red('/_____/ \\__,_/ /_/ /_/_\\__, /') + chalk.yellow(' \\__,_/ /_/  \\__,_/ /_/      ') +
-  '\n    ' + chalk.white.bgRed('                      /____/' + chalk.white.bgYellow('                       ')) +
+  '\n    ' + chalk.yellow('----------------------') + chalk.red('/____/-----------------------') +
+  '\n                                 ' + chalk.bgBlack('Light & Fast purposed') +
   '\n\n';
 
 module.exports = title;
