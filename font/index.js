0c9dbd1f39e84fb16854f0ed4977df4abc979f63
Aperçu <bgronon@gmail.com>
2014-09-22 03:04:29 +0200

    feat(subgen): font generator, fix package name

---
+++ font/index.js
@@ -0,0 +1,92 @@
+'use strict';
+
+var async = require('async');
+var chalk = require('chalk');
+var yeoman = require('yeoman-generator');
+var genUtils = require('../util');
+
+function bangLog (msg, color) {
+  console.log('[' + chalk.blue('Bangular') + ']: ' + chalk[color](msg));
+}
+
+var BangularGenerator = yeoman.generators.NamedBase.extend({
+
+  prompting: function () {
+    var done = this.async();
+
+    this.name = this._.slugify(this.name);
+    this.types = {};
+
+    this.prompt([{
+      type: 'checkbox',
+      name: 'types',
+      message: 'What font file do you have ?',
+      choices: [{
+        value: 'eot',
+        name: 'eot',
+        checked: true
+      }, {
+        value: 'woff',
+        name: 'woff',
+        checked: false
+      }, {
+        value: 'ttf',
+        name: 'ttf',
+        checked: false
+      }, {
+        value: 'svg',
+        name: 'svg',
+        checked: false
+      }]
+    }], function (props) {
+
+      props.types.forEach(function (type) {
+        this.types[type] = true;
+      }.bind(this));
+
+      done();
+    }.bind(this));
+  },
+
+  writing: function () {
+
+    this.mkdir('client/assets/fonts/' + this.name);
+
+    if (!genUtils.fileExists('client/styles/fonts.scss')) {
+
+      this.template('fonts.scss', 'client/styles/fonts.scss');
+
+      genUtils.rewriteFile({
+        file: 'client/styles/app.scss',
+        needle: '// Imports',
+        splicable: [
+          '@import \'fonts\';'
+        ]
+      });
+    } else {
+      this.template('fonts.scss', '.tmp/fonts.scss');
+
+      // For now we will use this since the template is created asynchronously and there is no callback.
+      async.retry(4, function (cb) {
+        setTimeout(function () {
+          try {
+            genUtils.appendTo(this, { src: '.tmp/fonts.scss', dest: 'client/styles/fonts.scss' });
+            cb(null, true);
+          } catch (e) {
+            cb(e, null);
+          }
+        }, 1);
+      }, function (err) {
+        if (err) {
+          bangLog('There was an error copying the template to your font file.', 'red');
+        } else {
+          bangLog('Your font was successfully injected.', 'green');
+        }
+      });
+
+    }
+
+  }
+});
+
+module.exports = BangularGenerator;
