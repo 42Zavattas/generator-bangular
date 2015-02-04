f9a32d0b1d059800aad7ae45853b8dc8c74579eb
Aperçu <bgronon@gmail.com>
2015-02-04 04:48:04 +0100

    feat(tests): add service tests

---
+++ test/test-service.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var path = require('path');
+var os = require('os');
+var helpers = require('yeoman-generator').test;
+var assert = require('yeoman-generator').assert;
+
+describe('Launching service tests', function () {
+
+  var bangular, bangService, tmpDir;
+  var bangDir = process.cwd();
+
+  describe('', function () {
+
+    before(function (done) {
+
+      tmpDir = path.join(os.tmpdir(), '/tmp');
+
+      helpers.testDirectory(tmpDir, function (err) {
+        bangular = helpers.createGenerator('bangular:app',
+          [path.join(bangDir, '/app')],
+        false, { 'skipInstall': true, 'skipLog': true });
+
+        helpers.mockPrompt(bangular, { name: 'Test', backend: 'json', modules: [] });
+        bangular.run(done);
+      });
+
+    });
+
+    it('should create a new service', function (done) {
+
+      bangService = helpers.createGenerator('bangular:service', [bangDir + '/service'], 'socket');
+      bangService.run(function () {
+        assert.file('client/services/socket/socket.js');
+        assert.file('client/services/socket/socket.spec.js');
+        assert.fileContent('client/services/socket/socket.js', '.service(\'Socket\', function () {');
+        assert.fileContent('client/services/socket/socket.spec.js', 'inject(function (_Socket_) {');
+        done();
+      });
+
+    });
+
+  });
+
+});
