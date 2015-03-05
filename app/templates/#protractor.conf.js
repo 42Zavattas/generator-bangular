17260f3201dbf201ac5c23a5fab8ef1c43d07c6e
Balthazar Gronon <bgronon@gmail.com>
2015-03-05 18:52:08 +0100

    fix(tests): change paths for karma and protractor

--- app/templates/#protractor.conf.js
+++ app/templates/#protractor.conf.js
@@ -1,15 +1,15 @@
 exports.config = { // jshint ignore:line
 
   // The version is susceptible to change
-  seleniumServerJar: '../node_modules/gulp-protractor/node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
-  // chromeDriver: '../node_modules/gulp-protractor/node_modules/protractor/selenium/chromedriver',
+  seleniumServerJar: './node_modules/gulp-protractor/node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
+  // chromeDriver: './node_modules/gulp-protractor/node_modules/protractor/selenium/chromedriver',
 
   baseUrl: 'http://localhost:9000',
 
   capabilities: {
     browserName: 'chrome'
     // chromeOptions: {
-    //    binary: '/usr/bin/google-chrome-unstable'
+    //    binary: '/usr/bin/google-chrome-stable'
     // }
   },
 
