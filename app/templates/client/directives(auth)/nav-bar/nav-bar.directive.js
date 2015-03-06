23d5c76fe10368ee132538a6850d2e20354c9cdf
meriadec <meriadec.pillet@gmail.com>
2015-03-06 16:43:14 +0100

    feat(app): prevent flickr effect for navbar, including it into directive

---
+++ app/templates/client/directives(auth)/nav-bar/nav-bar.directive.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('<%= appname %>')
+  .directive('navBar', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'directives/nav-bar/nav-bar.html'
+    };
+  });
