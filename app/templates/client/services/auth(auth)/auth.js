97800437527dc455aee986f54e27a279329f9e93
Balthazar Gronon <bgronon@gmail.com>
2015-05-01 11:38:13 +0200

    test(auth): update with new service

--- app/templates/client/services/auth(auth)/auth.js
+++ app/templates/client/services/auth(auth)/auth.js
@@ -11,7 +11,7 @@ angular.module('<%= appname %>')
         .then(function (res) {
           _user = res.data;
         })
-        .finally(function (err) {
+        .finally(function () {
           _ready.resolve();
         });
     } else {
