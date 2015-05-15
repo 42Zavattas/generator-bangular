3bf65ccf1a9077b6769ffca3ed5d9c3f2d4996c9
Balthazar Gronon <bgronon@gmail.com>
2015-05-15 11:14:30 +0200

    fix(inject): using angular sort plugin for injected files
    And remove useless style inject.
    
    Closes #21.

--- app/templates/tasks/config/#filesToInject.js
+++ app/templates/tasks/config/#filesToInject.js
@@ -9,6 +9,5 @@ module.exports = [
   'client/directives/**/*.js', '!client/directives/**/*.spec.js',
   'client/filters/**/*.js', '!client/filters/**/*.spec.js',
   'client/services/**/*.js', '!client/services/**/*.spec.js',
-  'client/views/**/*.js', '!client/views/**/*.spec.js', '!client/views/**/*.e2e.js',
-  'client/styles/css/app.css'
+  'client/views/**/*.js', '!client/views/**/*.spec.js', '!client/views/**/*.e2e.js'
 ];
--- app/templates/tasks/inject.js
+++ app/templates/tasks/inject.js
@@ -6,20 +6,21 @@
 
 var gulp       = require('gulp');
 var bowerFiles = require('main-bower-files');
+var fileSort   = require('gulp-angular-filesort');
 var inject     = require('gulp-inject');
 
 var toInject   = require('./config/filesToInject');
 var toExclude  = require('./config/bowerFilesToExclude');
 
 module.exports = function () {
-  var sources = gulp.src(toInject, { read: false });
-
   return gulp.src('client/index.html')
     .pipe(inject(gulp.src(bowerFiles(), { read: false }), {
       name: 'bower',
       relative: 'true',
       ignorePath: toExclude
     }))
-    .pipe(inject(sources, { relative: true }))
+    .pipe(inject(
+      gulp.src(toInject).pipe(fileSort()), { relative: true }
+    ))
     .pipe(gulp.dest('client'));
 };
