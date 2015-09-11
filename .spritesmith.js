5ed0f9d6ce48d79b81bd43b64b3e736a2f38fa31
Aperçu <bgronon@gmail.com>
2015-09-11 21:25:42 +0200

    refactor(core): remove gulp and upgrade deps

---
+++ .spritesmith.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  src: 'logos/!(logos-sprite).png',
+  destImage: './logos/logos-sprite.png',
+  padding: 10,
+  algorithm: 'left-right',
+  algorithmOpts: { sort: false }
+};
